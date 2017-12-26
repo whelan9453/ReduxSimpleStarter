ba2786f9dd2cac5f51443ce4be923026cdd98d5f
whelan957 <sitdtest@gmail.com>
2017-12-26 23:11:09 +0800

    Polish search bar component.

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor (props) {
+      super(props)
+      this.state = {term: ''}
+      //callbacks referenceing "this" need to bind the context
+      this.onInputChange = this.onInputChange.bind(this)
+  }
+
+  onInputChange(event) {
+    this.setState({term: event.target.value});
+  }
+
+  onFormSubmit(event) {
+    //Disable default submit behavior
+    event.preventDefault();
+
+    //TODO fetch weather data
+  }
+
   render() {
     return (
-      <form className="input-group">
-        <input />
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input 
+          placeholder="Get weather forecast here!"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn" >
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
